146bb2a51307a390e066e4a8657a75e30a9dc28c
MiguelGuevara25 <miguel_g1789@hotmail.com>
2024-06-07 21:46:11 -0500

    nueva página

---
+++ src/pages/index.js
@@ -0,0 +1,41 @@
+import AboutMeOne from "@/components/AboutSection/AboutMeOne";
+import FunFacts from "@/components/FunFacts/FunFacts";
+import GallerySectionOne from "@/components/GallerySection/GallerySectionOne";
+import HeaderOne from "@/components/Header/HeaderOne";
+import MobileMenu from "@/components/Header/MobileMenu";
+import Layout from "@/components/Layout/Layout";
+import SearchPopup from "@/components/SearchPopup/SearchPopup";
+
+const Home = () => {
+  return (
+    <Layout pageTitle="Home 01">
+      <HeaderOne
+        rightMenu
+        links={false}
+        headerStyle="header-style-one header-style-six header-style-six-no-border"
+        logo={2}
+      />
+      <MobileMenu />
+      <SearchPopup />
+
+      <div style={{ height: "100vh", display: "flex" }}>
+        <div className="w-100 d-flex justify-content-center align-items-center">
+          <h1>Real State</h1>
+        </div>
+
+        <div className="w-100">
+          <img
+            className="w-100"
+            src="https://linoorwp.pixydrops.com/wp-content/uploads/2023/02/slider-eleven-img-1.jpg"
+          />
+        </div>
+      </div>
+
+      <FunFacts />
+      <AboutMeOne />
+      <GallerySectionOne />
+    </Layout>
+  );
+};
+
+export default Home;
